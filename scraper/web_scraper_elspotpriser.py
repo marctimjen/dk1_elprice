d9188dd34662992647b839a68bc13a7dd8a57e6a
marctimjen <79468558+marctimjen@users.noreply.github.com>
2025-02-27 20:34:17 +0100

    REMOVE PROJECTROOT

--- scraper/web_scraper_elspotpriser.py
+++ scraper/web_scraper_elspotpriser.py
@@ -1,10 +1,6 @@
 import sys
 from pathlib import Path
 
-# Add project root to Python path
-project_root = str(Path(__file__).parent.parent)
-sys.path.append(project_root)
-
 import locale
 import platform
 import time as timenator
