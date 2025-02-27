6890308cb2ce44e5ce05d10734bcbc75edea3d20
marctimjen <79468558+marctimjen@users.noreply.github.com>
2025-02-27 17:32:13 +0100

    Update

--- process_data/main.py
+++ process_data/main.py
@@ -1,6 +1,15 @@
-from database.db_handler import DatabaseHandler
+import sys
 from datetime import datetime, timedelta
+from pathlib import Path
+
 import pytz
+# Add project root to Python path
+project_root = str(Path(__file__).parent.parent)
+sys.path.append(project_root)
+
+from database.db_handler import DatabaseHandler
+
+
 
 if __name__ == "__main__":
     db = DatabaseHandler()
--- scraper/scaper.py
+++ scraper/scaper.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+# Add project root to Python path
+project_root = str(Path(__file__).parent.parent)
+sys.path.append(project_root)
+
 from datetime import datetime, timedelta
 import pandas as pd
 import pytz
